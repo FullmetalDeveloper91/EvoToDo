bade83e5c4e50aff5f4e1c573b179820b419942a
Andrey Chudotvorov <lonelywolf796@gmail.com>
2025-04-13 14:19:55 +0700

    + JwtService

--- user-service/src/main/java/ru/fmd/user_service/user_service/config/SecurityConfig.java
+++ user-service/src/main/java/ru/fmd/user_service/user_service/config/SecurityConfig.java
@@ -2,15 +2,55 @@ package ru.fmd.user_service.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.fmd.user_service.user_service.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtFilter;
+
+    public SecurityConfig(UserDetailsService userDetailsService, JwtAuthenticationFilter jwtFilter) {
+        this.userDetailsService = userDetailsService;
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(
+                        requestMatchersRegistry -> requestMatchersRegistry
+                                .requestMatchers("/login/**", "/register/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .userDetailsService(userDetailsService)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
     @Bean
     PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(@NonNull AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
--- user-service/src/main/java/ru/fmd/user_service/user_service/service/JwtService.java
+++ user-service/src/main/java/ru/fmd/user_service/user_service/service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import ru.fmd.user_service.user_service.model.User;
 
@@ -31,10 +32,23 @@ public class JwtService {
     }
 
     public String extractUsername(String token){
-        return ExtractClaim(token, Claims::getSubject);
+        return extractClaim(token, Claims::getSubject);
     }
 
-    private <T> T ExtractClaim(String token, Function<Claims,T> resolver){
+    public boolean isValid(String token, UserDetails user){
+        String username = extractUsername(token);
+        return username.equals(user.getUsername()) && isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims,T> resolver){
         Claims claims = extractAllClaims(token);
 
         return resolver.apply(claims);
